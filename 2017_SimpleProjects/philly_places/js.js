41e784fcdc69cba72f867101c482d535341e3d59
wenhaowuuu <wenhaow@upenn.edu>
2017-07-20 11:44:09 -0400

    0720_1

---
+++ 2017_SimpleProjects/philly_places/js.js
@@ -0,0 +1,12 @@
+var map = L.map('map', {
+  center: [15.162820, -87.509107],
+  zoom: 6.5
+});
+
+var Style = 'light';
+
+L.tileLayer('http://{s}.basemaps.cartocdn.com/'+ Style + '_all/{z}/{x}/{y}@2x.png', {
+  maxZoom: 18,
+  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, &copy; <a href="https://carto.com/attribution">CARTO</a>',
+  subdomains: 'abcd'
+}).addTo(map);
